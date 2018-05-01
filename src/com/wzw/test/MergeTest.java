20b4952bfd14907286d5e583a263cf7753e45981
15817303442@163.com <15817303442@163.com>
2018-05-01 20:50:52 +0800

    conflict commit

--- src/com/wzw/test/MergeTest.java
+++ src/com/wzw/test/MergeTest.java
@@ -5,7 +5,7 @@ public class MergeTest {
 	public static void main(String[] args) {
 		System.out.println("merge core1......");
 		System.out.println("merge core2......");
-		System.out.println("merge core3 conflict......");
+		System.out.println("merge core3......");
 	}
 	
 }
