6917da5a55e1eda65dc2a6064170fab50ef326a6
15817303442@163.com <15817303442@163.com>
2018-05-01 20:57:20 +0800

    merge commit

--- src/com/wzw/test/MergeTest.java
+++ src/com/wzw/test/MergeTest.java
@@ -3,7 +3,7 @@ package com.wzw.test;
 public class MergeTest {
 	
 	public static void main(String[] args) {
-		System.out.println("merge core1......");
+		System.out.println("merge......");
 		System.out.println("conflict test......");
 	}
 	
